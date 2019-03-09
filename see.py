2358792e028fdc39c282c675d05d55cc1f2fe9c0
Zhijie Cao <caozj1017@hotmail.com>
2019-03-09 15:02:27 +0800

    add read_csv function and clear no use columns

--- see.py
+++ see.py
@@ -1,15 +1,28 @@
 # -*- coding: UTF-8 -*-
 #!/usr/local/bin/python3
 import pandas as pd
-import os,sys
+import os, sys
 pd.set_option('display.max_rows',None)
 dirname = os.path.dirname(os.path.realpath(__file__))
 x = ['[XIANGCB_BF].[dbo].[CAI_LIST_OLD].csv',
 '[XIANGCB_BF].[dbo].[ORDERS_old].csv',
 '[XIANGCB_BF].[dbo].[ZUOF_LISTTBL_OLD].csv',
 '[XIANGCB_BF].[dbo].[FANT_ZENGSONG].csv']
-filename = dirname + '/XIANGCB_BF/' + x[0]
+filename = dirname + '/data/use/' + x[0]
+del x
 
 print(filename)
-df = pd.read_csv(filename, encoding='gbk')
-df[:200].to_csv('a.csv')
+df = pd.read_csv(filename, encoding='gbk', infer_datetime_format=True)
+#df = df[:3000]
+
+no_use_col = ['HAS_DADAN','SF_SJ','SFDD_VAR',
+              'JSR_NAME','RNAM_VAR','ZHUOWEI_VAR',
+              'DASK_VAR','PRIN_VAR','SFWF_BIT',
+              'BT_PRICE','CJDW_VAR','FJFY_FLO',
+              'LBXH', 'BT_PRICE','CCSJ_DTE']
+df.drop(no_use_col,axis=1, inplace=True)
+del no_use_col
+# df.to_excel('a.xlsx')
+
+col = df.columns
+ODNO = df.ODNO_VAR.drop_duplicates() # len(ODNO) == 172183
